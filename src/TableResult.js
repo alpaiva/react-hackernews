c0f6e084b24f0f79307fb4f97b9e600cd0a81955
Andre <and.l.paiva@gmail.com>
2022-02-06 17:54:09 -0300

    added input text for Filter

--- src/TableResult.js
+++ src/TableResult.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Table, Button } from 'react-bootstrap'
+import { Table, Button, Form } from 'react-bootstrap'
 const list = [
     {
         title: 'React',
@@ -18,54 +18,80 @@ const list = [
         objectID: 1,
     },
 ]
+
+const isSearched = searchTerm => 
+    item => item.title.toLowerCase().includes(searchTerm.toLowerCase())
+
+
 class TableResult extends Component {
 
     constructor(props) {
         super(props)
 
-        this.state = { list }
+        this.state = {
+            list,
+            searchTerm: ''
+        }
 
         this.onDismiss = this.onDismiss.bind(this)
-        
+        this.onSearchChange = this.onSearchChange.bind(this)
+    
     }
 
     render() {
-        return (<Table reponsive='sm' striped bordered hover size='sm'>
-            <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Author</th>
-                    <th>Comments</th>
-                    <th>Points</th>
-                </tr>
-            </thead>
-            <tbody>
-                {this.state.list.map(item =>
-                    <tr key={item.objectID}>
-                        
-                        <td>
-                            <a href={item.url}>{item.title}</a>
-                        </td>
-                        <td>{item.author}</td>
-                        <td>{item.num_comments}</td>
-                        <td>{item.points}</td>
-                        <td>
-                            <Button variant='secondary' size='sm'
-                            onClick={() => this.onDismiss(item.objectID)} >
-                                Dismiss
-                            </Button>
-                        </td>
-                    </tr>
-                )
-                }
-            </tbody>
-        </Table>)
+        return (
+            <div>
+                <Form>
+                    <Form.Control className="mb-2" placeholder="Input filter" 
+                    onChange={this.onSearchChange} />
+                </Form>
+                <Table reponsive='sm' striped bordered hover size='sm'>
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Author</th>
+                            <th>Comments</th>
+                            <th>Points</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.list
+                        .filter(isSearched(this.state.searchTerm))
+                        .map(item =>
+                            <tr key={item.objectID}>
+
+                                <td>
+                                    <a href={item.url}>{item.title}</a>
+                                </td>
+                                <td>{item.author}</td>
+                                <td>{item.num_comments}</td>
+                                <td>{item.points}</td>
+                                <td>
+                                    <Button variant='secondary' size='sm'
+                                        onClick={() => this.onDismiss(item.objectID)} >
+                                        Dismiss
+                                    </Button>
+                                </td>
+                            </tr>
+                        )
+                        }
+                    </tbody>
+                </Table>
+            </div>
+        )
     }
 
     onDismiss(id) {
         const filteredList = this.state.list.filter(item => item.objectID !== id)
         this.setState({ list: filteredList })
     }
+
+    onSearchChange(event) {
+     
+        this.setState({searchTerm : event.target.value})
+    }
+
+    
 }
 
 export default TableResult
