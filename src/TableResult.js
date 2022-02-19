14907dd94e45039a90051848b1c3f3000be0cb1a
Andre <and.l.paiva@gmail.com>
2022-02-19 16:28:39 -0300

    class component changed to functional

--- src/TableResult.js
+++ src/TableResult.js
@@ -1,48 +1,47 @@
-import { Component } from 'react'
 import { Table, Button } from 'react-bootstrap'
 
 const isSearched = searchTerm =>
     item => item.title.toLowerCase().includes(searchTerm.toLowerCase())
-class TableResult extends Component {
-    render() {
-        const { pattern, list, onDismiss } = this.props
+const TableResult = props => {
 
-        return (
+    const { pattern, list, onDismiss } = props
 
-            <Table reponsive='sm' striped bordered hover size='sm'>
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Author</th>
-                        <th>Comments</th>
-                        <th>Points</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {list
-                        .filter(isSearched(pattern))
-                        .map(item =>
-                            <tr key={item.objectID}>
-                                <td>
-                                    <a href={item.url}>{item.title}</a>
-                                </td>
-                                <td>{item.author}</td>
-                                <td>{item.num_comments}</td>
-                                <td>{item.points}</td>
-                                <td>
-                                    <Button variant='secondary' size='sm'
-                                        onClick={() => onDismiss(item.objectID)} >
-                                        Dismiss
-                                    </Button>
-                                </td>
-                            </tr>
-                        )
-                    }
-                </tbody>
-            </Table>
+    return (
 
-        )
-    }
+        <Table reponsive='sm' striped bordered hover size='sm'>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Comments</th>
+                    <th>Points</th>
+                </tr>
+            </thead>
+            <tbody>
+                {list
+                    .filter(isSearched(pattern))
+                    .map(item =>
+                        <tr key={item.objectID}>
+                            <td>
+                                <a href={item.url}>{item.title}</a>
+                            </td>
+                            <td>{item.author}</td>
+                            <td>{item.num_comments}</td>
+                            <td>{item.points}</td>
+                            <td>
+                                <Button variant='secondary' size='sm'
+                                    onClick={() => onDismiss(item.objectID)} >
+                                    Dismiss
+                                </Button>
+                            </td>
+                        </tr>
+                    )
+                }
+            </tbody>
+        </Table>
+
+    )
+
 }
 
 export default TableResult
