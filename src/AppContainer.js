68c9fc992a6578951179f5cf1434115324cfd71c
Andre <and.l.paiva@gmail.com>
2022-03-01 11:30:02 -0300

    Added axios lib

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import TableResult from "./TableResult";
 import Search from './Search'
 import { Pagination } from "react-bootstrap";
+import axios from 'axios'
 
 
 const DEFAULT_QUERY = 'redux'
@@ -11,6 +12,8 @@ const PARAM_SEARCH = 'query=';
 const PARAM_PAGE = 'page='
 
 class AppContainer extends Component {
+
+    _isMounted = false
 
     constructor(props) {
         super(props)
@@ -29,18 +32,22 @@ class AppContainer extends Component {
     }
 
     componentDidMount() {
+        this._isMounted = true
         const { searchTerm } = this.state;
         this.setState({ searchKey: searchTerm })
         this.fetchSearchTopStories(searchTerm);
     }
 
+    componentWillUnmount() {
+        this._isMounted = false
+    }
+
     fetchSearchTopStories(searchTerm, page = 0) {
         
         const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`;
-        fetch(url)
-            .then(response => response.json())
-            .then(result => this.setSearchTopStories(result))
-            .catch(error => this.setState({ error }));
+        axios(url)
+            .then(result => this._isMounted && this.setSearchTopStories(result.data))
+            .catch(error => this._isMounted && this.setState({ error }));
     }
 
     setSearchTopStories(result) {
